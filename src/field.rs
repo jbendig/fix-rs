e6e1edc03707d5a0f1e185372f6ebc1b441d72ff
James Bendig <jbendig@starbytesoftware.com>
2016-10-18 15:04:45 -0500

    Added copyright and licensing info.

--- src/field.rs
+++ src/field.rs
@@ -1,3 +1,14 @@
+// Copyright 2016 James Bendig. See the COPYRIGHT file at the top-level
+// directory of this distribution.
+//
+// Licensed under:
+//   the MIT license
+//     <LICENSE-MIT or https://opensource.org/licenses/MIT>
+//   or the Apache License, Version 2.0
+//     <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>,
+// at your option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use message::Message;
 use constant::VALUE_END;
 use std::any::Any;
