6b619327d58171efc5fedf7fb6c6eaaabdf248c5
James Bendig <jbendig@starbytesoftware.com>
2016-12-21 17:20:15 -0600

    Added forgotten copyright header to recently added files.

--- src/dictionary/field_types/mod.rs
+++ src/dictionary/field_types/mod.rs
@@ -1,2 +1,13 @@
+// Copyright 2016 James Bendig. See the COPYRIGHT file at the top-level
+// directory of this distribution.
+//
+// Licensed under:
+//   the MIT license
+//     <LICENSE-MIT or https://opensource.org/licenses/MIT>
+//   or the Apache License, Version 2.0
+//     <LICENSE-APACHE or https://www.apache.org/licenses/LICENSE-2.0>,
+// at your option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 pub mod generic;
 pub mod other;
