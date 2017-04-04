90713d28e954635f414085d5ac98d6db5e018bbe
James Bendig <jbendig@starbytesoftware.com>
2017-04-04 13:32:15 -0500

    Added TestRequest latency command to fix-rs-lt.

--- src/bin/fix-rs-lt.rs
+++ src/bin/fix-rs-lt.rs
@@ -22,6 +22,7 @@ use clap::{App,Arg};
 use mio::{Events,Poll,PollOpt,Ready,Token};
 use mio::tcp::TcpStream;
 use std::any::Any;
+use std::collections::HashMap;
 use std::net::{Ipv4Addr,SocketAddr,SocketAddrV4};
 use std::io::{self,Read,Write};
 use std::marker::PhantomData;
@@ -40,13 +41,13 @@ use fix_rs::field_type::FieldType;
 use fix_rs::fix::Parser;
 use fix_rs::fix_version::FIXVersion;
 use fix_rs::fixt;
-use fix_rs::fixt::message::FIXTMessage;
+use fix_rs::fixt::message::{BuildFIXTMessage,FIXTMessage};
 use fix_rs::message::{self,REQUIRED,NOT_REQUIRED,Message,SetValueError};
 use fix_rs::message_version::{self,MessageVersion};
 
 const SEND_MESSAGE_TIMEOUT_SECS: u64 = 10;
 const MAX_MESSAGE_SIZE: u64 = 4096;
-const MESSAGE_COUNT: u64 = 1000;
+const MESSAGE_COUNT: u64 = 1_000_000;
 
 define_fields!(
     EmptySenderCompID: EmptyFieldType = 49,
@@ -120,11 +121,19 @@ impl FIXTMessage for Heartbeat {
         unimplemented!();
     }
 
+    fn set_is_poss_dup(&mut self,_is_poss_dup: bool) {
+        unimplemented!();
+    }
+
     fn sending_time(&self) -> <<SendingTime as Field>::Type as FieldType>::Type {
         unimplemented!();
     }
 
     fn orig_sending_time(&self) -> <<OrigSendingTime as Field>::Type as FieldType>::Type {
+        unimplemented!();
+    }
+
+    fn set_orig_sending_time(&mut self,_orig_sending_time: <<OrigSendingTime as Field>::Type as FieldType>::Type) {
         unimplemented!();
     }
 
@@ -176,11 +185,19 @@ impl FIXTMessage for TestRequest {
         unimplemented!();
     }
 
+    fn set_is_poss_dup(&mut self,_is_poss_dup: bool) {
+        unimplemented!();
+    }
+
     fn sending_time(&self) -> <<SendingTime as Field>::Type as FieldType>::Type {
         unimplemented!();
     }
 
     fn orig_sending_time(&self) -> <<OrigSendingTime as Field>::Type as FieldType>::Type {
+        unimplemented!();
+    }
+
+    fn set_orig_sending_time(&mut self,_orig_sending_time: <<OrigSendingTime as Field>::Type as FieldType>::Type) {
         unimplemented!();
     }
 
@@ -203,6 +220,26 @@ struct LatencyResult {
     end_parse_time: Instant,
 }
 
+fn print_statistics(total_duration: Duration,latency_results: Vec<LatencyResult>) {
+    let mut sum_latency = Duration::new(0,0);
+    let mut min_latency = Duration::new(u64::max_value(),0);
+    let mut max_latency = Duration::new(u64::min_value(),u32::min_value());
+    for latency_result in &latency_results {
+        let latency = latency_result.end_parse_time - latency_result.begin_send_time;
+
+        sum_latency += latency;
+        min_latency = std::cmp::min(min_latency,latency);
+        max_latency = std::cmp::max(max_latency,latency);
+    }
+    let average_latency = sum_latency / latency_results.len() as u32;
+
+    println!("Sent {} TestRequest messages",MESSAGE_COUNT);
+    println!("  Total duration: {:?}",total_duration);
+    println!("  Minimum Latency: {:?}",min_latency);
+    println!("  Average latency: {:?}",average_latency);
+    println!("  Maximum Latency: {:?}",max_latency);
+}
+
 struct Connection {
     poll: Poll,
     stream: TcpStream,
@@ -212,6 +249,39 @@ struct Connection {
 }
 
 impl Connection {
+    pub fn connect_and_logon(message_dictionary: HashMap<&'static [u8],Box<BuildFIXTMessage + Send>>) -> Result<Connection,io::Error> {
+        //Connect to server.
+        let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127,0,0,1),7001));
+        let mut connection = Connection {
+            poll: Poll::new().unwrap(),
+            stream: TcpStream::connect(&addr).unwrap(),
+            parser: Parser::new(message_dictionary,MAX_MESSAGE_SIZE),
+            outbound_buffer: ByteBuffer::with_capacity(1024),
+            inbound_buffer: ByteBuffer::with_capacity(16384),
+        };
+
+        connection.poll.register(&connection.stream,Token(0),Ready::all(),PollOpt::edge()).unwrap();
+
+        //Logon.
+        let mut logon_message = Logon::new();
+        logon_message.setup_fixt_session_header(
+            Some(1),
+            b"fix-rs-lt".to_vec(),
+            b"Server".to_vec()
+        );
+        logon_message.encrypt_method = EncryptMethod::None;
+        logon_message.heart_bt_int = 60;
+        logon_message.default_appl_ver_id = MessageVersion::FIX50SP2;
+        logon_message.username = b"some_user".to_vec();
+        logon_message.password = b"some_password".to_vec();
+        try!(connection.send_message(logon_message));
+        try!(connection.recv_message::<Logon>().map(|_|()));
+
+        Ok(connection)
+    }
+
+    /// Try to send an entire message within SEND_MESSAGE_TIMEOUT_SECS seconds. Failing to send all
+    /// bytes before the timeout triggers a panic.
     fn send_message<T: FIXTMessage + Any + Send>(&mut self,message: T) -> Result<(),io::Error> {
         let mut bytes = Vec::new();
         message.read(FIXVersion::FIXT_1_1,MessageVersion::FIX50SP2,&mut bytes);
@@ -240,19 +310,13 @@ impl Connection {
         Ok(())
     }
 
-    fn send_all_messages<'a, T: Iterator<Item=&'a TestRequest>,F>(&mut self,iter: &mut T,mut sending_message_func: F) -> Result<(),io::Error> 
+    /// Send all messages until iter is empty or writing to socket would block.
+    pub fn send_all_messages<'a, T: Iterator<Item=&'a TestRequest>,F>(&mut self,iter: &mut T,mut sending_message_func: F) -> Result<(),io::Error>
         where F: FnMut(&TestRequest) {
         loop {
+            self.prepare_send_message(iter,&mut sending_message_func);
             if self.outbound_buffer.is_empty() {
-                if let Some(next_message) = iter.next() {
-                    self.outbound_buffer.clear_and_read_all(|ref mut bytes| {
-                        next_message.read(FIXVersion::FIXT_1_1,MessageVersion::FIX50SP2,bytes);
-                    });
-                    sending_message_func(&next_message);
-                }
-                else {
-                    return Ok(());
-                }
+                break;
             }
 
             match self.outbound_buffer.write(&mut self.stream) {
@@ -265,9 +329,44 @@ impl Connection {
                 },
             }
         }
+
+        Ok(())
     }
 
-    fn recv_fixt_message(&mut self) -> Result<Box<FIXTMessage + Send>,io::Error> {
+    /// Send remaining output bytes or as much as possible of next message.
+    pub fn send_next_message<'a, T: Iterator<Item=&'a TestRequest>,F>(&mut self,iter: &mut T,mut sending_message_func: F) -> Result<(),io::Error>
+        where F: FnMut(&TestRequest) {
+        self.prepare_send_message(iter,&mut sending_message_func);
+        if self.outbound_buffer.is_empty() {
+            return Ok(());
+        }
+
+        match self.outbound_buffer.write(&mut self.stream) {
+            Ok(_) => {},
+            Err(e) => {
+                if e.kind() == ::std::io::ErrorKind::WouldBlock {
+                    return Ok(());
+                }
+                return Err(e);
+            },
+        }
+
+        Ok(())
+    }
+
+    fn prepare_send_message<'a, T: Iterator<Item=&'a TestRequest>,F>(&mut self,iter: &mut T,mut sending_message_func: F)
+        where F: FnMut(&TestRequest) {
+        if self.outbound_buffer.is_empty() {
+            if let Some(next_message) = iter.next() {
+                self.outbound_buffer.clear_and_read_all(|ref mut bytes| {
+                    next_message.read(FIXVersion::FIXT_1_1,MessageVersion::FIX50SP2,bytes);
+                });
+                sending_message_func(&next_message);
+            }
+        }
+    }
+
+    pub fn recv_fixt_message(&mut self) -> Result<Box<FIXTMessage + Send>,io::Error> {
         if !self.parser.messages.is_empty() {
             return Ok(self.parser.messages.remove(0));
         }
@@ -312,7 +411,7 @@ impl Connection {
         Ok(fixt_message.as_any().downcast_ref::<T>().expect("Not expected message type").clone())
     }
 
-    fn recv_all_messages<F>(&mut self,mut received_message_func: F) -> Result<(),io::Error>
+    pub fn recv_all_messages<F>(&mut self,mut received_message_func: F) -> Result<(),io::Error>
         where F: FnMut(&Box<FIXTMessage + Send>) {
         loop {
             match self.inbound_buffer.clear_and_read(&mut self.stream) {
@@ -343,89 +442,66 @@ impl Connection {
     }
 }
 
-fn test_request() -> Result<(),io::Error> {
-    //Use an iterator to generate messages that will be sent.
-    struct TestRequestIter<'a> {
-        index: u64,
-        count: u64,
-        test_request_message: TestRequest,
-        _marker: PhantomData<&'a TestRequest>,
-    }
+//Use an iterator to generate messages that will be sent.
+struct TestRequestIter<'a> {
+    index: u64,
+    count: u64,
+    test_request_message: TestRequest,
+    _marker: PhantomData<&'a TestRequest>,
+}
 
-    impl<'a> TestRequestIter<'a> {
-        fn with_start_msg_seq_num(start_msg_seq_num: u64) -> Self {
-            let mut test_request_message = TestRequest::new();
-            test_request_message.setup_fixt_session_header(
-                Some(start_msg_seq_num - 1),
-                b"fix-rs-lt".to_vec(),
-                b"Server".to_vec());
+impl<'a> TestRequestIter<'a> {
+    fn with_start_msg_seq_num(start_msg_seq_num: u64) -> Self {
+        let mut test_request_message = TestRequest::new();
+        test_request_message.setup_fixt_session_header(
+            Some(start_msg_seq_num - 1),
+            b"fix-rs-lt".to_vec(),
+            b"Server".to_vec());
 
-            TestRequestIter {
-                index: 0,
-                count: 1000000,
-                test_request_message: test_request_message,
-                _marker: PhantomData,
-            }
-        }
-
-        fn with_message_count(mut self,count: u64) -> Self {
-            self.count = count;
-            self
+        TestRequestIter {
+            index: 0,
+            count: 1000000,
+            test_request_message: test_request_message,
+            _marker: PhantomData,
         }
     }
 
-    impl<'a> Iterator for TestRequestIter<'a> {
-        type Item = &'a TestRequest;
-        fn next(&mut self) -> Option<Self::Item> {
-            if self.index >= self.count {
-                return None;
-            }
+    fn with_message_count(mut self,count: u64) -> Self {
+        self.count = count;
+        self
+    }
+}
 
-            self.test_request_message.msg_seq_num += 1;
-            self.test_request_message.test_req_id = self.index.to_string().as_bytes().to_vec();
-            self.index += 1;
-            unsafe {
-                //Yup, not safe. But this makes sure the message is never cloned or copied which is
-                //super expensive.
-                return Some(mem::transmute(&self.test_request_message as *const TestRequest));
-            }
+impl<'a> Iterator for TestRequestIter<'a> {
+    type Item = &'a TestRequest;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.count {
+            return None;
+        }
+
+        self.test_request_message.msg_seq_num += 1;
+        self.test_request_message.test_req_id = self.index.to_string().as_bytes().to_vec();
+        self.index += 1;
+        unsafe {
+            //Yup, not safe. But this makes sure the message is never cloned or copied which is
+            //super expensive.
+            return Some(mem::transmute(&self.test_request_message as *const TestRequest));
         }
     }
+}
 
+fn test_request_load() -> Result<(),io::Error> {
     define_dictionary!(
         Logon,
         TestRequest,
         Heartbeat,
     );
 
-    //Connect to server.
     let mut latency_results = vec![LatencyResult { begin_send_time: Instant::now(), end_parse_time: Instant::now() };MESSAGE_COUNT as usize];
-    let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127,0,0,1),7001));
-    let mut connection = Connection {
-        poll: Poll::new().unwrap(),
-        stream: TcpStream::connect(&addr).unwrap(),
-        parser: Parser::new(build_dictionary(),MAX_MESSAGE_SIZE),
-        outbound_buffer: ByteBuffer::with_capacity(1024),
-        inbound_buffer: ByteBuffer::with_capacity(16384),
-    };
+    let mut connection = try!(Connection::connect_and_logon(build_dictionary()));
 
-    connection.poll.register(&connection.stream,Token(0),Ready::all(),PollOpt::edge()).unwrap();
-
-    //Logon.
-    let mut logon_message = Logon::new();
-    logon_message.setup_fixt_session_header(
-        Some(1),
-        b"fix-rs-lt".to_vec(),
-        b"Server".to_vec());
-    logon_message.encrypt_method = EncryptMethod::None;
-    logon_message.heart_bt_int = 60;
-    logon_message.default_appl_ver_id = MessageVersion::FIX50SP2;
-    logon_message.username = b"some_user".to_vec();
-    logon_message.password = b"some_password".to_vec();
-    try!(connection.send_message(logon_message));
-    try!(connection.recv_message::<Logon>().map(|_|()));
-
-    //Send TestRequest messages and measure how long it takes to get a response for each.
+    //Send TestRequest messages with a priority on sending over receiving. Then measure how long it
+    //takes to get a response for each.
     let mut iter = TestRequestIter::with_start_msg_seq_num(2).with_message_count(MESSAGE_COUNT);
     let mut events = Events::with_capacity(8);
     let start_instant = Instant::now();
@@ -441,7 +517,7 @@ fn test_request() -> Result<(),io::Error> {
                     //TODO: Maybe check the test_req_id instead to be more general?
                     latency_results[message.msg_seq_num() as usize - 2].begin_send_time = Instant::now();
                 }));
-            }
+           }
 
             if event.kind().is_readable() {
                 try!(connection.recv_all_messages(|ref message| {
@@ -459,26 +535,61 @@ fn test_request() -> Result<(),io::Error> {
             }
         }
     }
-    let total_duration = start_instant.elapsed();
 
     //Calculate and print statistics.
-    let mut sum_latency = Duration::new(0,0);
-    let mut min_latency = Duration::new(u64::max_value(),0);
-    let mut max_latency = Duration::new(u64::min_value(),u32::min_value());
-    for latency_result in &latency_results {
-        let latency = latency_result.end_parse_time - latency_result.begin_send_time;
+    let total_duration = start_instant.elapsed();
+    print_statistics(total_duration,latency_results);
 
-        sum_latency += latency;
-        min_latency = std::cmp::min(min_latency,latency);
-        max_latency = std::cmp::max(max_latency,latency);
+    return Ok(());
+}
+
+fn test_request_latency() -> Result<(),io::Error> {
+    define_dictionary!(
+        Logon,
+        TestRequest,
+        Heartbeat,
+    );
+
+    let mut latency_results = vec![LatencyResult { begin_send_time: Instant::now(), end_parse_time: Instant::now() };MESSAGE_COUNT as usize];
+    let mut connection = try!(Connection::connect_and_logon(build_dictionary()));
+
+    //Send TestRequest messages with a priority on receiving over sending. Then measure how long it
+    //takes to get a response for each.
+    let mut iter = TestRequestIter::with_start_msg_seq_num(2).with_message_count(MESSAGE_COUNT);
+    let mut events = Events::with_capacity(8);
+    let start_instant = Instant::now();
+    let mut running = true;
+    while running {
+        if let Err(_) = connection.poll.poll(&mut events,None) {
+            panic!("Poll failed");
+        }
+
+        for event in events.iter() {
+            if event.kind().is_readable() {
+                try!(connection.recv_all_messages(|ref message| {
+                    latency_results[message.msg_seq_num() as usize - 2].end_parse_time = Instant::now();
+
+                    //Have all messages been received?
+                    if message.msg_seq_num() >= MESSAGE_COUNT + 1 {
+                        running = false;
+                    }
+                }));
+            }
+
+            if event.kind().is_hup() {
+                panic!("Other side closed connection");
+            }
+
+            try!(connection.send_next_message(&mut iter,|ref message| {
+                //TODO: Maybe check the test_req_id instead to be more general?
+                latency_results[message.msg_seq_num() as usize - 2].begin_send_time = Instant::now();
+            }));
+        }
     }
-    let average_latency = sum_latency / latency_results.len() as u32;
 
-    println!("Sent {} TestRequest messages",MESSAGE_COUNT);
-    println!("  Total duration: {:?}",total_duration);
-    println!("  Minimum Latency: {:?}",min_latency);
-    println!("  Average latency: {:?}",average_latency);
-    println!("  Maximum Latency: {:?}",max_latency);
+    //Calculate and print statistics.
+    let total_duration = start_instant.elapsed();
+    print_statistics(total_duration,latency_results);
 
     return Ok(());
 }
@@ -492,7 +603,7 @@ fn main() {
                                  .required(true)
                                  .index(1)
                                  .takes_value(true)
-                                 .possible_values(&["test_request",]))
+                                 .possible_values(&["test_request_load","test_request_latency"]))
                        .get_matches();
 
     //TODO: Make message count adjustable.
@@ -502,7 +613,8 @@ fn main() {
     //TODO: Make sender_comp_id and target_comp_id adjustable.
 
     let result = match matches.value_of("type").unwrap() {
-        "test_request" => test_request(),
+        "test_request_load" => test_request_load(),
+        "test_request_latency" => test_request_latency(),
         _ => panic!("Not a supported type"),
     };
 
